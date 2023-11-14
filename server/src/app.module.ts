8fe9ba6fb434fb2e814487fd2f40561283ed63aa
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2023-11-13 23:31:03 -0800

    Server/feature/#13 (#25)
    * chore: postgresql, nestjs docker 세팅
    
    * chore: @nestjs/typeorm, typeorm, pg 설치
    
    * chore: Typeorm 세팅 및 TestModel 테이블 생성

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
+import {TestModel} from "./app.entity";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env['DB_HOST'],
+      port: parseInt(process.env['DB_PORT']),
+      username: process.env['DB_USERNAME'],
+      password: process.env['DB_PASSWORD'],
+      database: process.env['DB_DATABASE'],
+      entities: [TestModel],
+      synchronize: true, // DO NOT USE IN PRODUCTION
+    }),
+    TypeOrmModule.forFeature([TestModel]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
