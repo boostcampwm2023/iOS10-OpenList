6c5f4398e88e8d7cb2a9abc211a7311a505d319a
dongsuk <scienceyds@naver.com>
2023-11-15 14:22:52 +0900

    feature: base entity 구현

---
+++ server/src/common/entity/base.entity.ts
@@ -0,0 +1,16 @@
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class BaseModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
