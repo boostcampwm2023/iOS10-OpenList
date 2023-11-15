6e43a1ef3e18e506535c697e0c518f6ab2a28f13
wi-seong-cheol <tjd9248@naver.com>
2023-11-15 22:47:53 +0900

    feat: Network 모듈 구성 및 테스트

---
+++ client/Projects/CustomNetwork/CustomNetwork/RequestRetrier.swift
@@ -0,0 +1,29 @@
+//
+//  RequestRetrier.swift
+//  CustomNetwork
+//
+//  Created by wi_seong on 11/14/23.
+//
+
+import Foundation
+
+public protocol RequestRetrier {
+	func shouldRetry(_ request: URLRequest, with error: Error, attempt: Int) -> Bool
+	func retry(_ request: URLRequest, with error: Error, attempt: Int) -> URLRequest?
+}
+
+public final class SimpleRequestRetrier: RequestRetrier {
+	private let maxAttempts: Int
+	
+	public init(maxAttempts: Int = 3) {
+		self.maxAttempts = maxAttempts
+	}
+	
+	public func shouldRetry(_ request: URLRequest, with error: Error, attempt: Int) -> Bool {
+		return attempt < maxAttempts
+	}
+	
+	public func retry(_ request: URLRequest, with error: Error, attempt: Int) -> URLRequest? {
+		return request
+	}
+}
