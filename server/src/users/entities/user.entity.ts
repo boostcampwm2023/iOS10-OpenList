5ef62ed14f3bdf84637a68f2e96977ab4f3fe186
dongsuk <scienceyds@naver.com>
2023-11-15 14:49:49 +0900

    feature: userEntity 이메일 필드 추가

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -3,6 +3,9 @@ import { Column, Entity, Generated } from 'typeorm';
 
 @Entity()
 export class UsersModel extends BaseModel {
+  @Column({ unique: true })
+  email: string;
+
   @Column()
   @Generated('uuid') // 임시로 uuid를 생성해줌(원래는 provider의 고유 id를 받아와야함)
   providerId: string;
