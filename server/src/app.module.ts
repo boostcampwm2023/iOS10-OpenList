88a01cf3aa984c5f61769cc7bf6eaed7213b98aa
dongsuk <scienceyds@naver.com>
2023-11-15 14:34:38 +0900

    feature: 모듈에 usersModel 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {ConfigModule} from "@nestjs/config";
-import {TestModel} from "./app.entity";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { TestModel } from './app.entity';
 import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
+import { UsersModel } from './users/entities/users.entity';
 
 @Module({
   imports: [
@@ -20,7 +21,7 @@ import { UsersModule } from './users/users.module';
       username: process.env['DB_USERNAME'],
       password: process.env['DB_PASSWORD'],
       database: process.env['DB_DATABASE'],
-      entities: [TestModel],
+      entities: [TestModel, UsersModel],
       synchronize: true, // DO NOT USE IN PRODUCTION
     }),
     TypeOrmModule.forFeature([TestModel]),
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModel } from './entities/users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsersModel])],
   controllers: [UsersController],
   providers: [UsersService],
 })
