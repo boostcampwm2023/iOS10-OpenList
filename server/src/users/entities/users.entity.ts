51cea8ca94e8032a7c1e8f9ce73015f16c7d1ee0
dongsuk <scienceyds@naver.com>
2023-11-15 14:31:53 +0900

    feature: usersEntity 구현

---
+++ server/src/users/entities/users.entity.ts
@@ -0,0 +1,18 @@
+import { BaseModel } from 'src/common/entity/base.entity';
+import { Column, Entity, Generated } from 'typeorm';
+
+@Entity()
+export class UsersModel extends BaseModel {
+  @Column()
+  @Generated('uuid') // 임시로 uuid를 생성해줌(원래는 provider의 고유 id를 받아와야함)
+  providerId: string;
+
+  @Column({ default: 'APPLE' })
+  provider: string;
+
+  @Column()
+  nickname: string;
+
+  @Column({ default: 'testimagelink' })
+  profileImage: string;
+}
