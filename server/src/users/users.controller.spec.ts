e434e10fd2dbf70146167614f65f3925ff594588
dongsuk <scienceyds@naver.com>
2023-11-15 20:21:46 +0900

    feature: users.controller.spec.ts 의존성 주입

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TestCommonModule } from 'test/test-common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModel } from './entities/user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TestCommonModule, TypeOrmModule.forFeature([UsersModel])],
       controllers: [UsersController],
       providers: [UsersService],
     }).compile();
