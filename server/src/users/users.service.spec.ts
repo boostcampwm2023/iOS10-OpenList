2b6fb53ca613746e7b78743cf2a12d51c0570700
YangDongsuk <51476641+YangDongsuk@users.noreply.github.com>
2023-11-15 20:37:09 +0900

    [Server] 유닛 테스트 환경 세팅 (#32)
    * chore: test 경로 설정
    
    * feature: TestCommonModule 구현
    
    * feature: users.service.spec.ts 의존성 주입
    
    * feature: users.controller.spec.ts 의존성 주입

--- server/src/users/users.service.spec.ts
+++ server/src/users/users.service.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { TestCommonModule } from 'test/test-common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModel } from './entities/user.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -7,6 +10,7 @@ describe('UsersService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService],
+      imports: [TestCommonModule, TypeOrmModule.forFeature([UsersModel])],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
