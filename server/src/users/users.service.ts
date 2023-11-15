ca87a761221a730c48118256c23f7a3f23eba2f8
dongsuk <scienceyds@naver.com>
2023-11-15 15:13:01 +0900

    feature : create user 구현

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersModel } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(UsersModel)
+    private readonly usersRepository: Repository<UsersModel>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    const userObject = this.usersRepository.create(createUserDto);
+    const emailExists = await this.usersRepository.exist({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+    if (emailExists) {
+      throw new BadRequestException('이미 존재하는 이메일입니다.');
+    }
+    const newUser = await this.usersRepository.save(userObject);
+    return newUser;
   }
 
   findAll() {
