edd58e113b5391eb7959bd506913c74dfd990c04
dongsuk <scienceyds@naver.com>
2023-11-15 20:52:21 +0900

    style: usersController 네이밍에 컨벤션 맞게 변경

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -17,27 +17,27 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  postUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  getUsers() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  getUser(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  putUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  deleteUser(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
 }
