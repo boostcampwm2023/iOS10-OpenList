0bbba5f325fdf86ad224b159e72d789140d0a2f8
dongsuk <scienceyds@naver.com>
2023-11-15 15:05:23 +0900

    feature: updateUserDto 구현

--- server/src/users/dto/update-user.dto.ts
+++ server/src/users/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PickType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PickType(CreateUserDto, ['nickname']) {}
