6f26a35cfdd621f01e0c5bda38a8827783df01e5
Minseong Park <pminsung12@gmail.com>
2023-11-19 03:30:26 +0900

    fix: class 이름 오타 수정

--- server/src/checklists/dto/update-private-checklist.dto.ts
+++ server/src/checklists/dto/update-private-checklist.dto.ts
@@ -1,7 +1,7 @@
 import { PickType } from '@nestjs/mapped-types';
 import { PrivateChecklistModel } from '../entities/private-checklist.entity';
 
-export class CreatePrivateChecklistDto extends PickType(PrivateChecklistModel, [
+export class UpdatePrivateChecklistDto extends PickType(PrivateChecklistModel, [
   'title',
   'progress',
   'folder',
