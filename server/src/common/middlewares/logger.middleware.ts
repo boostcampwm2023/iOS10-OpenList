a463ffa43c4b85c49053f3c4118c69a7e4ee351b
Minseong Park <pminsung12@gmail.com>
2023-11-23 01:38:02 +0900

    feat: 로그에 요청 처리 시간 추가되도록 개선

--- server/src/common/middlewares/logger.middleware.ts
+++ server/src/common/middlewares/logger.middleware.ts
@@ -8,16 +8,16 @@ export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = WinstonModule.createLogger(winstonConfig);
 
   use(req: Request, res: Response, next: NextFunction) {
-    // 요청 객체로부터 ip, http method, url, user agent를 받아온 후
+    const startTime = Date.now(); // 요청 시작 시간 기록
     const { ip, method, originalUrl } = req;
     const userAgent = req.get('user-agent');
 
-    // 응답이 끝나는 이벤트가 발생하면 로그를 찍는다.
     res.on('finish', () => {
+      const duration = Date.now() - startTime; // 요청 처리 시간 계산
       const { statusCode } = res;
       this.logger.log({
         level: 'info',
-        message: `${method} ${originalUrl} ${statusCode} ${ip} ${userAgent}`,
+        message: `${method} ${originalUrl} ${statusCode} ${ip} ${userAgent} - ${duration}ms`,
       });
     });
 
