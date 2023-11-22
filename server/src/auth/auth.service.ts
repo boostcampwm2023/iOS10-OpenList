8019d26b914edc846df213f21c5bd3c68965ba89
dongsuk <scienceyds@naver.com>
2023-11-22 21:27:40 +0900

    feature: signToken 구현

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserModel } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
+type TokenType = 'access' | 'refresh';
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  /**
+   * 유저 정보를 통해 access/refresh 토큰을 발급한다.
+   * @param user 유저 정보
+   * @param tokenType 토큰 타입 (access/refresh)
+   * @returns 토큰
+   */
+  signToken(user: Pick<UserModel, 'email' | 'id'>, tokenType: TokenType) {
+    const payload = {
+      email: user.email,
+      userID: user.id,
+      tokenType: tokenType,
+    };
+    return this.jwtService.sign(payload, {
+      secret: process.env.JWT_SECRET,
+      expiresIn: tokenType === 'access' ? 300 : 3600,
+    });
+  }
+
   create(createAuthDto: CreateAuthDto) {
     return 'This action adds a new auth';
   }
