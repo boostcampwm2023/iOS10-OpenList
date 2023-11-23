2a1b2bc61c734b712da10b4df161659f2ba3fb91
wi-seong-cheol <tjd9248@naver.com>
2023-11-23 17:49:28 +0900

    feat: UseCase 구현

--- client/Projects/OpenList/OpenList/Domain/UseCases/CRDTUseCase.swift
+++ client/Projects/OpenList/OpenList/Domain/UseCases/CRDTUseCase.swift
@@ -5,4 +5,55 @@
 //  Created by wi_seong on 11/23/23.
 //
 
-import Foundation
+import CRDT
+
+protocol CRDTUseCase {
+	func receiveData(data: Node) async throws -> String?
+	func insert(range: Range<String.Index>) async throws -> CRDTMessage
+	func delete(range: Range<String.Index>) async throws -> CRDTMessage
+}
+
+final class DefaultCRDTUseCase {
+	private let crdtRepository: CRDTRepository
+	private let document: RGASDocument<String> = .init()
+	private lazy var merge: RGASMerge<String> = .init(doc: document, siteID: 0)
+	
+	init(crdtRepository: CRDTRepository) {
+		self.crdtRepository = crdtRepository
+	}
+}
+
+extension DefaultCRDTUseCase: CRDTUseCase {
+	func receiveData(data: Node) async throws -> String? {
+		guard data.id == Device.id else { return nil }
+		try data.message.execute(on: merge)
+		return document.view()
+	}
+	
+	func insert(range: Range<String.Index>) async throws -> CRDTMessage {
+		let operation = createOperation(range: range, type: .insert)
+		let message = try merge.applyLocal(to: operation)
+		return try await crdtRepository.save(message: message)
+	}
+	
+	func delete(range: Range<String.Index>) async throws -> CRDTMessage {
+		let operation = createOperation(range: range, type: .delete)
+		let message = try merge.applyLocal(to: operation)
+		return try await crdtRepository.save(message: message)
+	}
+}
+
+private extension DefaultCRDTUseCase {
+	func createOperation(
+		range: Range<String.Index>,
+		type: OpType
+	) -> SequenceOperation<String> {
+		let operation = SequenceOperation(
+			type: type,
+			position: 0,
+			argument: 1,
+			content: [""]
+		)
+		return operation
+	}
+}
--- client/Projects/OpenList/OpenList/Domain/UseCases/WebSocketUseCase.swift
+++ client/Projects/OpenList/OpenList/Domain/UseCases/WebSocketUseCase.swift
@@ -5,4 +5,46 @@
 //  Created by wi_seong on 11/23/23.
 //
 
+import CRDT
 import Foundation
+
+protocol WebSocketUseCase {
+	func connect() -> Bool
+	func send(data: Data)
+	func send(message: CRDTMessage)
+	func receive() async -> Node
+}
+
+final class DefaultWebSocketUseCase {
+	private let webSocketRepository: WebSocketRepository
+	
+	init(webSocketRepository: WebSocketRepository) {
+		self.webSocketRepository = webSocketRepository
+	}
+}
+
+extension DefaultWebSocketUseCase: WebSocketUseCase {
+	func connect() -> Bool {
+		guard let data = Device.id.data(using: .utf8) else {
+			return false
+		}
+		webSocketRepository.send(data: data)
+		return true
+	}
+	
+	func send(data: Data) {
+		webSocketRepository.send(data: data)
+	}
+	
+	func send(message: CRDTMessage) {
+		let node = Node(id: Device.id, message: message)
+		guard let data = try? JSONEncoder().encode(node) else {
+			return
+		}
+		webSocketRepository.send(data: data)
+	}
+	
+	func receive() async -> Node {
+		return await webSocketRepository.receive()
+	}
+}
