942f506a0d675dbd8ea9a9e55756ed0e15819316
wi-seong-cheol <tjd9248@naver.com>
2023-11-23 17:48:14 +0900

    feat: CRDT, WebSocket Repository 구현

--- client/Projects/OpenList/OpenList/Data/Repositories/DefaultWebSocketRepository.swift
+++ client/Projects/OpenList/OpenList/Data/Repositories/DefaultWebSocketRepository.swift
@@ -5,4 +5,60 @@
 //  Created by wi_seong on 11/23/23.
 //
 
+import CRDT
+import CustomSocket
 import Foundation
+
+final class DefaultWebSocketRepository: NSObject {
+	private let webSocket: WebSocket
+	
+	init(url: URL) {
+		let webSocket = WebSocket()
+		webSocket.url = url
+		self.webSocket = webSocket
+		super.init()
+		setUp()
+	}
+	
+	func setUp() {
+		webSocket.delegate = self
+		try? webSocket.openWebSocket()
+	}
+}
+
+extension DefaultWebSocketRepository: WebSocketRepository {
+	func send(data: Data) {
+		webSocket.send(data: data)
+	}
+	
+	func receive() async -> Node {
+		return await withCheckedContinuation { continuation in
+			webSocket.receive(onReceive: { (_, data) in
+				guard
+					let data,
+					let response = try? JSONDecoder().decode(Node.self, from: data)
+				else { return }
+				continuation.resume(returning: response)
+			})
+		}
+	}
+}
+
+extension DefaultWebSocketRepository: URLSessionWebSocketDelegate {
+	func urlSession(
+		_ session: URLSession,
+		webSocketTask: URLSessionWebSocketTask,
+		didOpenWithProtocol protocol: String?
+	) {
+		print("open")
+	}
+	
+	func urlSession(
+		_ session: URLSession,
+		webSocketTask: URLSessionWebSocketTask,
+		didCloseWith closeCode: URLSessionWebSocketTask.CloseCode,
+		reason: Data?
+	) {
+		print("close")
+	}
+}
