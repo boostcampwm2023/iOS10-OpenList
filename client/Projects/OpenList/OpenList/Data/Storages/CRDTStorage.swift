676b3a53ee397d26493bdcae0f4c40710f56ca2c
wi-seong-cheol <tjd9248@naver.com>
2023-11-23 17:46:54 +0900

    feat: WebSocket 구현

---
+++ client/Projects/OpenList/OpenList/Data/Storages/CRDTStorage.swift
@@ -0,0 +1,8 @@
+//
+//  CRDTStorage.swift
+//  OpenList
+//
+//  Created by wi_seong on 11/23/23.
+//
+
+import Foundation
---
+++ client/Projects/OpenList/OpenList/Domain/Entites/Device.swift
@@ -0,0 +1,8 @@
+//
+//  Device.swift
+//  OpenList
+//
+//  Created by wi_seong on 11/23/23.
+//
+
+import Foundation
