4c9438546b3f489a677e12788fc380912b0ad607
wi-seong-cheol <tjd9248@naver.com>
2023-11-27 17:24:39 +0900

    feat: 동시편집 로직 구현

--- client/Projects/OpenList/OpenList/Data/Repositories/DefaultCRDTRepository.swift
+++ client/Projects/OpenList/OpenList/Data/Repositories/DefaultCRDTRepository.swift
@@ -6,6 +6,7 @@
 //
 
 import CRDT
+import CustomSocket
 
 final class DefaultCRDTRepository {
 	private let crdtStorage: CRDTStorage
@@ -19,6 +20,12 @@ extension DefaultCRDTRepository: CRDTRepository {
 	func save(message: CRDTMessage) async throws -> CRDTMessage {
 		try await crdtStorage.save(message: message)
 		return message
+	}
+	
+	func send(documentNode: LinkedListNode<UUID>, message: CRDTMessage) throws {
+		let request = CRDTRequestDTO(event: Device.id, documentNode: documentNode, data: message)
+		let data = try JSONEncoder().encode(request)
+		WebSocket.shared.send(data: data)
 	}
 	
 	func fetchAll() async throws -> [CRDTMessage] {
