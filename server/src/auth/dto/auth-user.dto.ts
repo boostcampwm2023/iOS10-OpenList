870493d956991119188a9dd5826c79f711fdee45
Minseong Park <pminsung12@gmail.com>
2023-11-28 01:10:30 +0900

    feat: request body로 들어오는 auth-user.dto.ts 수정

--- server/src/auth/dto/auth-user.dto.ts
+++ server/src/auth/dto/auth-user.dto.ts
@@ -1,42 +1,11 @@
-import {
-  IsString,
-  IsEmail,
-  IsNotEmpty,
-  ValidateNested,
-  IsOptional,
-} from 'class-validator';
-import { Type } from 'class-transformer';
-
-class NameDto {
-  @IsString()
-  @IsNotEmpty()
-  firstName: string;
-
-  @IsString()
-  @IsNotEmpty()
-  lastName: string;
-}
-
-export class UserDto {
-  @IsEmail()
-  email: string;
-
-  @ValidateNested()
-  @Type(() => NameDto)
-  name: NameDto;
-}
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class AuthUserDto {
   @IsString()
   @IsNotEmpty()
-  authorizationCode: string;
-
-  @IsString()
-  @IsNotEmpty()
-  idToken: string;
+  identityToken: string;
 
   @IsOptional()
-  @ValidateNested()
-  @Type(() => UserDto)
-  user?: UserDto;
+  @IsString()
+  fullName?: string;
 }
