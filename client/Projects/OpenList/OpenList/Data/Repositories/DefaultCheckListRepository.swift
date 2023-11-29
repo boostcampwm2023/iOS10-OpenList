30eb985840cf94ff2e71a104e451f04c036c673a
wi-seong-cheol <tjd9248@naver.com>
2023-11-29 14:46:15 +0900

    fix: 체크리스트 진행률 오류 수정

--- client/Projects/OpenList/OpenList/Data/Repositories/DefaultCheckListRepository.swift
+++ client/Projects/OpenList/OpenList/Data/Repositories/DefaultCheckListRepository.swift
@@ -28,10 +28,11 @@ extension DefaultCheckListRepository: CheckListRepository {
 	func fetchAllCheckList() async throws -> [CheckListTableItem] {
 		let result = try await checkListStorage.fetchAllCheckList()
 		return result.map {
+			let achievementRate = ($0.items.isEmpty ? 0 : Double($0.progress) / Double($0.items.count))
 			return CheckListTableItem(
 				id: $0.checklistId,
 				title: $0.title,
-				achievementRate: Double($0.progress) / Double($0.items.count)
+				achievementRate: achievementRate
 			)
 		}
 	}
