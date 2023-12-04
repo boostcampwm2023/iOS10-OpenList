feb66da4fcdab668ef630ccd8745606d63ac7246
Minseong Park <pminsung12@gmail.com>
2023-12-05 02:41:16 +0900

    feat: 로그에 한국 시간대 추가

--- server/src/common/middlewares/logger.middleware.ts
+++ server/src/common/middlewares/logger.middleware.ts
@@ -8,16 +8,19 @@ export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = WinstonModule.createLogger(winstonConfig);
 
   use(req: Request, res: Response, next: NextFunction) {
-    const startTime = Date.now(); // 요청 시작 시간 기록
+    const startTime = new Date(); // 현재 시간을 Date 객체로 얻기
+    const startTimeString = startTime.toLocaleString('ko-KR', {
+      timeZone: 'Asia/Seoul',
+    }); // 한국 시간대로 변환
     const { ip, method, originalUrl } = req;
     const userAgent = req.get('user-agent');
 
     res.on('finish', () => {
-      const duration = Date.now() - startTime; // 요청 처리 시간 계산
+      const duration = Date.now() - startTime.getTime(); // 요청 처리 시간 계산
       const { statusCode } = res;
       this.logger.log({
         level: 'info',
-        message: `${method} ${originalUrl} ${statusCode} ${ip} ${userAgent} - ${duration}ms`,
+        message: `${startTimeString} - ${method} ${originalUrl} ${statusCode} ${ip} ${userAgent} - ${duration}ms`,
       });
     });
 
