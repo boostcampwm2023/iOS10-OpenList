684cd4a6c75ad032944c3f1c335bcd882c0a940c
Minseong Park <pminsung12@gmail.com>
2023-12-06 02:50:49 +0900

    feat: 랜덤성을 높일 수 있는 temperature 옵션 설정

--- ai/generate-server/generate-server.js
+++ ai/generate-server/generate-server.js
@@ -43,6 +43,7 @@ export const generateGptData = async (category, count = 20) => {
     ],
     // model: 'gpt-3.5-turbo-1106',
     model: 'gpt-4-1106-preview',
+    temperature: 1.5, // randomness
     response_format: { type: 'json_object' },
   });
   const response = completion.choices[0];
