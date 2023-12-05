b25204798b96a00f22b2556b15c14d727ebd9137
dongsuk <scienceyds@naver.com>
2023-12-05 22:16:16 +0900

    feat: postgres 디비 연결

---
+++ ai/evaluate-server/postgres.js
@@ -0,0 +1,29 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+});
+
+// 쿼리 실행 예시
+pool.query("SELECT * FROM test_table", (err, res) => {
+  console.log(err, res);
+  console.log(res.rows);
+  pool.end();
+});
+
+// INSERT, UPDATE, DELETE 쿼리 실행 예시
+const text = "INSERT INTO your_table(name, email) VALUES($1, $2) RETURNING *";
+const values = ["brianc", "brian.m.carlson@gmail.com"];
+
+// pool.query(text, values, (err, res) => {
+//   if (err) {
+//     console.log(err.stack);
+//   } else {
+//     console.log(res.rows[0]);
+//   }
+// });
