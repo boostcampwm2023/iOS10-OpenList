aa8640d42c4a75d30199146d79125cedd702d373
dongsuk <scienceyds@naver.com>
2023-12-07 00:58:02 +0900

    feat: redis pub 서비스 추가

--- server/redis/redis.service.ts
+++ server/redis/redis.service.ts
@@ -6,11 +6,16 @@ export class RedisService {
   constructor(
     @Inject('REDIS_SUB_CLIENT')
     private readonly redisSubscriber: RedisClientType,
+    @Inject('REDIS_PUB_CLIENT')
+    private readonly redisPublisher: RedisClientType,
   ) {}
 
   subscribeToChannel(channel: string, callback: Function) {
     this.redisSubscriber.subscribe(channel, (message) => {
       callback(message);
     });
+  }
+  publishToChannel(channel: string, message: string) {
+    this.redisPublisher.publish(channel, message);
   }
 }
