5d4557b9dd4ec6587b7d7ca305702fa4a5ced189
Minseong Park <pminsung12@gmail.com>
2023-12-09 23:08:12 +0900

    chore: sub 모듈 env 방식으로 변경

--- ai/generate-server/redis-sub.js
+++ ai/generate-server/redis-sub.js
@@ -1,54 +1,63 @@
 import redis from 'redis';
+import { RedisPub } from './redis-pub.js';
+import { PromisePool } from '@supercharge/promise-pool';
 import dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
-
 import { generateGptData } from './generate-server.js';
 import { saveData } from './gpt-data-saver.js';
 
-import { RedisPub } from './redis-pub.js';
+dotenv.config({ path: '.env' });
 
 const subscriber = redis.createClient({
-  host: 'localhost',
-  port: 6379,
+  url: process.env.REDIS_URL,
 });
 
+/**
+ * 카테고리를 받아서 gpt-4로 데이터 생성 후 postgres에 저장
+ * @param category
+ * @returns {Promise<void>}
+ */
+async function processCategory(category) {
+  try {
+    const checklistItems = await generateGptData(category);
+    await saveData(checklistItems, category);
+  } catch (error) {
+    console.error(`Error processing category ${category}:`, error);
+  }
+}
+
+/**
+ * redis pub/sub을 통해 promise pool 방식으로 카테고리를 받아서 gpt-4로 데이터 생성 후 postgres에 저장
+ * @returns {Promise<void>}
+ */
 async function init() {
   const publisher = new RedisPub();
   const redisSub = await subscriber.connect();
 
   redisSub.subscribe('category', async function (message) {
-    try {
-      console.log('message:', message);
+    console.log('message:', message);
 
-      const parsedMessage = JSON.parse(message);
+    const parsedMessage = JSON.parse(message);
 
-      if (parsedMessage.messageData === 'generateGptData') {
-        const { messageData, categories } = parsedMessage;
-        console.log('messageData:', messageData);
-        console.log('categories:', categories);
+    if (parsedMessage.messageData === 'generateGptData') {
+      const { messageData, categories } = parsedMessage;
+      console.log('messageData:', messageData);
+      console.log('categories:', categories);
 
-        console.log('start');
+      console.log('start');
 
-        // generateGptData 함수 호출에 category 전달
-        for (const category of categories) {
-          const checklistItems = await generateGptData(category);
-          console.log('checklistItems:', checklistItems);
-          console.log('save to postgres');
-          // saveData 함수 호출에 category 전달
-          await saveData(checklistItems, category);
-          console.log('finished');
-        }
-        // const checklistItems = await generateGptData(category);
-        // console.log('checklistItems:', checklistItems);
-        // console.log('save to postgres');
-        // // saveData 함수 호출에 category 전달
-        // await saveData(checklistItems, category);
-        // console.log('finished');
+      // promise pool 방식으로 processCategory 함수 실행
+      const { results, errors } = await PromisePool.withConcurrency(5)
+        .for(categories)
+        .process(async (category) => {
+          await processCategory(category);
+        });
 
-        console.log('end');
+      // 오류 처리 또는 결과 확인
+      if (errors.length) {
+        console.error('Errors:', errors);
       }
-    } catch (error) {
-      console.error(`Error while processing ${message}: `, error);
+
+      console.log('end');
     }
   });
 }
