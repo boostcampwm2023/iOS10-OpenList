8c84d1002981233384b7b19a6d2e71dd499cd65b
Minseong Park <pminsung12@gmail.com>
2023-12-11 02:54:43 +0900

    feat: generate-item api에 대한 controller 함수 구현

--- server/src/checklist-ai/checklist-ai.controller.ts
+++ server/src/checklist-ai/checklist-ai.controller.ts
@@ -1,5 +1,5 @@
 import { ChecklistAiService } from './checklist-ai.service';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CreateChecklistItemsDto } from './dto/create-checklist-items.dto';
 
 @Controller('checklist-ai')
@@ -11,8 +11,14 @@ export class ChecklistAiController {
     return this.checklistAiService.generateChecklistItemWithAi(dto);
   }
 
-  @Get('item-count')
+  @Get('count-item')
   async getAiItemCount() {
-    return this.checklistAiService.publishItemCountByCategory();
+    return this.checklistAiService.publishToCountItemsByCategory();
+  }
+
+  // query param으로 count 가지고 옴.
+  @Post('generate-item')
+  async postAiItemGenerate(@Query('count') count: number) {
+    return this.checklistAiService.publishToGenerateItemsLessThanCount(count);
   }
 }
