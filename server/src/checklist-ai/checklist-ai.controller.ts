9d5ff29437ad38025d3757203aad0251fc04fc14
Minseong Park <52368015+pminsung12@users.noreply.github.com>
2023-12-11 13:08:12 +0900

    [Server] 카테고리별 아이템 개수 확인 및 개수로 item 생성 로직 (#228)
    * feat: 카테고리별 아이템 개수 반환해주는 api 생성
    
    * feat: 카테고리별 아이템 개수 itemCount로 pub도 하고 응답도 반환하는 서비스 함수 구현
    
    * feat: generate-item api에 대한 controller 함수 구현
    
    * feat: generate-item api에 대한 service 함수 구현

--- server/src/checklist-ai/checklist-ai.controller.ts
+++ server/src/checklist-ai/checklist-ai.controller.ts
@@ -1,5 +1,5 @@
 import { ChecklistAiService } from './checklist-ai.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CreateChecklistItemsDto } from './dto/create-checklist-items.dto';
 
 @Controller('checklist-ai')
@@ -9,5 +9,16 @@ export class ChecklistAiController {
   @Post()
   async getChecklistItemsWithAi(@Body() dto: CreateChecklistItemsDto) {
     return this.checklistAiService.generateChecklistItemWithAi(dto);
+  }
+
+  @Get('count-item')
+  async getAiItemCount() {
+    return this.checklistAiService.publishToCountItemsByCategory();
+  }
+
+  // query param으로 count 가지고 옴.
+  @Post('generate-item')
+  async postAiItemGenerate(@Query('count') count: number) {
+    return this.checklistAiService.publishToGenerateItemsLessThanCount(count);
   }
 }
--- server/src/checklist-ai/checklist-ai.service.ts
+++ server/src/checklist-ai/checklist-ai.service.ts
@@ -1,15 +1,29 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, ServiceUnavailableException } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  ServiceUnavailableException,
+} from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import * as process from 'process';
 import { CreateChecklistItemsDto } from './dto/create-checklist-items.dto';
 import { AI_OPTIONS } from './const/ai-options.const';
 import { SYSTEM_ROLE } from './const/system-role.const';
 import { CLOVA_API_URL } from './const/request-option.const';
+import { RedisClientType } from 'redis';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AiChecklistItemModel } from './entities/ai-checklist-item';
 
 @Injectable()
 export class ChecklistAiService {
-  constructor(private httpService: HttpService) {}
+  constructor(
+    private httpService: HttpService,
+    @InjectRepository(AiChecklistItemModel)
+    private readonly aiChecklistItemModelRepository: Repository<AiChecklistItemModel>,
+    @Inject('REDIS_PUB_CLIENT')
+    private readonly redisPublisher: RedisClientType,
+  ) {}
 
   // HTTP 요청을 보내는 별도의 메서드
   private async sendRequestToClova(url: string, headers: any, data: any) {
@@ -63,5 +77,86 @@ export class ChecklistAiService {
     }
 
     return this.extractResultData(response.data);
+  }
+
+  async getAiItemCountByCategories() {
+    const categoriesCount = await this.aiChecklistItemModelRepository
+      .createQueryBuilder('item')
+      .select('category.mainCategory', 'mainCategory')
+      .addSelect('category.subCategory', 'subCategory')
+      .addSelect('category.minorCategory', 'minorCategory')
+      .addSelect('COUNT(item.aiChecklistItemId)', 'itemCount')
+      .innerJoin('item.category', 'category')
+      .groupBy('category.mainCategory')
+      .addGroupBy('category.subCategory')
+      .addGroupBy('category.minorCategory')
+      .getRawMany();
+
+    console.log('categoriesCount', categoriesCount);
+    return categoriesCount;
+  }
+
+  async getCategoriesLessThanCount(count: number) {
+    const categories = await this.aiChecklistItemModelRepository
+      .createQueryBuilder('item')
+      .select('category.mainCategory', 'main')
+      .addSelect('category.subCategory', 'sub')
+      .addSelect('category.minorCategory', 'minor')
+      .addSelect('category.categoryId', 'categoryId')
+      .addSelect('COUNT(item.aiChecklistItemId)', 'itemCount') // 여기서 별칭 지정
+      .innerJoin('item.category', 'category')
+      .groupBy('category.categoryId')
+      .having('COUNT(item.aiChecklistItemId) < :count', { count }) // 집계 함수 사용
+      .getRawMany();
+
+    const formattedCategories = categories.map((cat) => [
+      cat.main,
+      cat.sub,
+      cat.minor,
+      cat.categoryId,
+    ]);
+
+    if (formattedCategories.length === 0) {
+      throw new ServiceUnavailableException('조건에 맞는 카테고리가 없습니다.');
+    }
+    this.redisPublisher.publish(
+      'ai_generate',
+      JSON.stringify({
+        messageData: `${count}개 이하의 아이템을 가진 카테고리 ${formattedCategories.length}목록`,
+        categories: formattedCategories,
+      }),
+    );
+
+    return formattedCategories;
+  }
+
+  formatCategoryData(categoriesCount) {
+    return categoriesCount.map((category) => {
+      return `[${category.mainCategory}, ${category.subCategory}, ${category.minorCategory}] - ${category.itemCount}개`;
+    });
+  }
+
+  publishToRedis(channel, messageData, formattedData) {
+    console.log('formattedData', formattedData);
+    this.redisPublisher.publish(
+      channel,
+      JSON.stringify({
+        messageData: messageData,
+        categories: formattedData,
+      }),
+    );
+  }
+
+  async publishToCountItemsByCategory() {
+    const categoriesCount = await this.getAiItemCountByCategories();
+    const formattedData = this.formatCategoryData(categoriesCount);
+    this.publishToRedis('itemCount', 'itemCount', formattedData);
+    return formattedData;
+  }
+
+  async publishToGenerateItemsLessThanCount(count: number) {
+    const categories = await this.getCategoriesLessThanCount(count);
+    this.publishToRedis('ai_generate', 'generateGptData', categories);
+    return categories;
   }
 }
