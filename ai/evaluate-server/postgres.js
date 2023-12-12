a18e3de026b06b473fba3c803b2791fc51f7436d
yangdongsuk <51476641+yangdongsuk@users.noreply.github.com>
2023-12-13 02:12:43 +0900

    feat: ai evaluate server 생성 (#239)
    * chore: gitignore 세팅
    
    * feat: evaluate api 구현
    
    * feat: ai 답변이 형식과 다를 경우 최대 3번까지 재시도 로직 구현
    
    * chore: typeorm, posgres 설치
    
    * feat: postgres 디비 연결
    
    * feat: array to object 함수 추가
    
    * chore: redis 라이브러리 추가
    
    * feat: 레디스 pub/sub 구현
    
    * feat: pub하면 인공지능 평가 실행 구현
    
    * feat: redis pub 클래스화
    
    * feat: 디비 기능 추가
    
    * feat: redis 연결
    
    * feat: 쿼리 및 결과물 파싱 함수 추가
    
    * feature: api,main 연결
    
    * feat: 평가 후 결과를 DB에 저장하는 기능 추가
    
    * feat: ai 자동화 api 구현
    
    * feat: 평가 api redis sub 연결
    
    * refactor : 불필요한 파일/코드 제거
    
    * style: 주석 추가
    
    * refactor: postgres 쿼리 변수화
    
    * feat: 최종 점수 매기는 기능 추가

---
+++ ai/evaluate-server/postgres.js
@@ -0,0 +1,70 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+});
+
+const ITEM_MODEL = "ai_checklist_item_model";
+const CATEGORY_MODEL = "category_model";
+const CATEGORY_ID = "categoryId";
+const REASON_MODEL = "ai_checklist_item_naver_reason_model";
+const AICHECKLISTITEMID = "aiChecklistItemId";
+const AICHECKLISTITEMSID = "aiChecklistItemAiChecklistItemId";
+
+async function getChecklistItemsByEvaluateCount(evaluateCount) {
+  const query = `
+    SELECT "${ITEM_MODEL}"."${AICHECKLISTITEMID}" AS item_id, *, ${evaluateCount} - evaluated_count_by_naver_ai AS diff
+    FROM "${ITEM_MODEL}" 
+    LEFT JOIN "${CATEGORY_MODEL}" ON "${ITEM_MODEL}"."${CATEGORY_ID}" = "${CATEGORY_MODEL}"."${CATEGORY_ID}"
+    WHERE evaluated_count_by_naver_ai < ${evaluateCount}
+    ORDER BY item_id;
+  `;
+  const result = await pool.query(query);
+  return result.rows;
+}
+
+async function incrementCounts(ids, type) {
+  const column =
+    type === "selected"
+      ? "selected_count_by_naver_ai"
+      : "evaluated_count_by_naver_ai";
+
+  const query = `
+    UPDATE "${ITEM_MODEL}"
+    SET "${column}" = "${column}" + 1
+    WHERE "${AICHECKLISTITEMID}" IN (${ids});
+  `;
+  await pool.query(query);
+}
+
+async function insertReasons(reasons) {
+  const inserts = Object.entries(reasons).map(([id, reason]) =>
+    pool.query(
+      `INSERT INTO "${REASON_MODEL}" ("${AICHECKLISTITEMSID}", reason) VALUES ($1, $2);`,
+      [id, reason]
+    )
+  );
+
+  await Promise.all(inserts);
+}
+
+async function setFinalScore() {
+  const query = `
+  UPDATE "${ITEM_MODEL}"
+  SET final_score = ROUND((selected_count_by_naver_ai::float / GREATEST(evaluated_count_by_naver_ai, 1)) * 100 + LOG(GREATEST(evaluated_count_by_naver_ai, 1)));
+  `;
+  await pool.query(query);
+}
+
+module.exports = {
+  getChecklistItemsByEvaluateCount,
+  incrementCounts,
+  insertReasons,
+  setFinalScore,
+  pool,
+};
