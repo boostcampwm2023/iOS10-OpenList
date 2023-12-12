7056ba1cfe9041a740aa34768b2e8d80b204678f
Minseong Park <pminsung12@gmail.com>
2023-12-12 23:28:33 +0900

    feat: category service 함수 수정

--- server/src/categories/categories.service.ts
+++ server/src/categories/categories.service.ts
@@ -1,14 +1,25 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { CATEGORIES } from './const/categories.const';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryModel } from './entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoriesService {
+  constructor(
+    @InjectRepository(CategoryModel)
+    private readonly categoryModelRepository: Repository<CategoryModel>,
+  ) {}
+
   /**
-   * 모든 대카테고리 반환
-   * @returns {{id: number, name: string}[]}
+   * 대카테고리 반환
+   * @returns {Promise<{id: number, name: string}[]>}
    */
-  findMainCategories(): { id: number; name: string }[] {
-    return CATEGORIES.mainCategory.map(({ id, name }) => ({ id, name }));
+  async findMainCategories(): Promise<{ id: number; name: string }[]> {
+    const categories = await this.categoryModelRepository.find();
+    return categories.map((category) => ({
+      id: category.categoryId,
+      name: category.mainCategory,
+    }));
   }
 
   /**
@@ -16,17 +27,16 @@ export class CategoriesService {
    * @param {number} mainId
    * @returns {{id: number, name: string}[]}
    */
-  findSubCategories(mainId: number): { id: number; name: string }[] {
-    // 특정 대카테고리의 중카테고리만 반환
-    const mainCategory = CATEGORIES.mainCategory.find(
-      (category) => category.id === (mainId as unknown as number),
-    );
-    if (!mainCategory) {
-      throw new BadRequestException(
-        `대 카테고리 ID ${mainId}은/는 존재하지 않습니다.`,
-      );
-    }
-    return mainCategory.subcategories.map(({ id, name }) => ({ id, name }));
+  async findSubCategories(
+    mainId: number,
+  ): Promise<{ id: number; name: string }[]> {
+    const categories = await this.categoryModelRepository.find({
+      where: { mainCategory: mainId.toString() },
+    });
+    return categories.map((category) => ({
+      id: category.categoryId,
+      name: category.subCategory,
+    }));
   }
 
   /**
@@ -35,26 +45,16 @@ export class CategoriesService {
    * @param {number} subId
    * @returns {{id: number; name: string}[]}
    */
-  findMinorCategories(
+  async findMinorCategories(
     mainId: number,
     subId: number,
-  ): { id: number; name: string }[] {
-    const mainCategory = CATEGORIES.mainCategory.find(
-      (category) => category.id === (mainId as unknown as number),
-    );
-    if (!mainCategory) {
-      throw new BadRequestException(
-        `대 카테고리 ID ${mainId}은/는 존재하지 않습니다.`,
-      );
-    }
-    const subCategory = mainCategory.subcategories.find(
-      (sub) => sub.id === (subId as unknown as number),
-    );
-    if (!subCategory) {
-      throw new BadRequestException(
-        `중 카테고리 ID ${subId}은/는 존재하지 않습니다.`,
-      );
-    }
-    return subCategory.minorCategories;
+  ): Promise<{ id: number; name: string }[]> {
+    const categories = await this.categoryModelRepository.find({
+      where: { mainCategory: mainId.toString(), subCategory: subId.toString() },
+    });
+    return categories.map((category) => ({
+      id: category.categoryId,
+      name: category.minorCategory,
+    }));
   }
 }
