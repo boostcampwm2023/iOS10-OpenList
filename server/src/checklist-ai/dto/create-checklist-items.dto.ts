954f0f88100e7d3e2f55b995eb9e0e735e9f80ce
Minseong Park <pminsung12@gmail.com>
2023-12-13 02:22:43 +0900

    feat: ai 체크리스트 항목 응답 api db 캐싱 구조로 변경

--- server/src/checklist-ai/dto/create-checklist-items.dto.ts
+++ server/src/checklist-ai/dto/create-checklist-items.dto.ts
@@ -1,15 +1,5 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
-const mainCategories = [
-  //체크리스트 대 카테고리
-  '일상',
-  '여행',
-  '취미',
-  '공부',
-  '운동',
-  '기타',
-];
-
 export class CreateChecklistItemsDto {
   // mainCategory에 있는 값은 mainCategories에 있는 값만 들어올 수 있도록
   // @IsIn(mainCategories)
