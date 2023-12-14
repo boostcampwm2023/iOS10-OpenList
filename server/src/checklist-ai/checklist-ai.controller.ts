1368aaa26a418062d0447a96e9f294f915ea8ab6
dongsuk <scienceyds@naver.com>
2023-12-14 17:21:43 +0900

    fix: 체크리스트 ai 생성 관리자만 가능하도록 수정

--- server/src/checklist-ai/checklist-ai.controller.ts
+++ server/src/checklist-ai/checklist-ai.controller.ts
@@ -1,5 +1,13 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { UserId } from 'src/users/decorator/userId.decorator';
 import { ChecklistAiService } from './checklist-ai.service';
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CreateChecklistItemsDto } from './dto/create-checklist-items.dto';
 
 @Controller('checklist-ai')
@@ -12,13 +20,22 @@ export class ChecklistAiController {
   }
 
   @Get('count-item')
-  async getAiItemCount() {
+  async getAiItemCount(@UserId() userId: number) {
+    if (userId !== 1) {
+      throw new UnauthorizedException('관리자가 아닙니다.');
+    }
     return this.checklistAiService.publishToCountItemsByCategory();
   }
 
   // query param으로 count 가지고 옴.
   @Post('generate-item')
-  async postAiItemGenerate(@Query('count') count: number) {
+  async postAiItemGenerate(
+    @Query('count') count: number,
+    @UserId() userId: number,
+  ) {
+    if (userId !== 1) {
+      throw new UnauthorizedException('관리자가 아닙니다.');
+    }
     return this.checklistAiService.publishToGenerateItemsLessThanCount(count);
   }
 }
